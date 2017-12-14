1b93f2fbb7dcac9a8e20b9b319e00a00aacc7505
soundofthecity <34424103+soundofthecity@users.noreply.github.com>
2017-12-14 16:18:39 +0300

    Update feature_extraction.py

--- Code/feature_extraction.py
+++ Code/feature_extraction.py
@@ -20,9 +20,9 @@ def extract_features(raw_sound, sample_rate):
 
 
 sample_rates = np.load("sample_rates.npy")
-labels = np.load("soundlabels.npy")
+labels = np.load("raw_sounds_id.npy")
 
-s1 = np.load("raw_sounds/0.npy")
+s1 = np.load("raw_data/0.npy")
 m, c, mel, cn, tn = extract_features(s1, sample_rates[0])
 numsample = len(sample_rates)
 
@@ -35,20 +35,25 @@ tonnets = np.empty((numsample, tn.shape[0]))
 
 def take_raw_sounds(path):
     index = 0
-    for raw_sound in os.listdir(path):
-        raw = np.load(path+raw_sound)
-        mx, cx, melx, cnx, tnx = extract_features(raw, sample_rates[index])
-        mfccs[index] = mx
-        chromas[index] = cx
-        mels[index] = melx
-        contrasts[index] = cnx
-        tonnets[index] = tnx
+
+    for raw_sound in range(8072):
+        raw = np.load(path + str(raw_sound) + '.npy')
+
+        try:
+            mx, cx, melx, cnx, tnx = extract_features(raw, sample_rates[index])
+            mfccs[index] = mx
+            chromas[index] = cx
+            mels[index] = melx
+            contrasts[index] = cnx
+            tonnets[index] = tnx
+        except:
+            print(path, raw_sound, ".npy")
 
         index += 1
         print(index)
 
 
-take_raw_sounds("raw_sounds/")
+take_raw_sounds("raw_data/")
 
 np.save("mfccs.npy", mfccs)
 np.save("chromas.npy", chromas)
